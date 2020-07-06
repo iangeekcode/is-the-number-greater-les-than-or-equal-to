375b350923b59c34e36944c3f7c556376ea175d3
iangeekcode <ialexnardo@gmail.com>
2020-07-06 17:19:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+number= int(input("Tell me a number one throught ten"))
+
+if number > 10:
+    print("Your number is greater than ten")
+    
+elif number <10:
+    print("Your number is less than ten")
+    
+else:
+    print("Your number is equal to ten")
